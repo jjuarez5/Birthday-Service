560aea982eda02566597d27035c8cb85bb4db5db
jjuarez5 <julio.juarez5@outlook.com>
2023-05-27 19:48:44 -0500

    create birthday response

---
+++ BirthdayResponse.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace Birthday_Service
+{
+    public class BirthdayResponse
+    {
+        [JsonProperty(PropertyName="name")]
+        public string Name { get; set; }
+
+
+        [JsonProperty(PropertyName="birthday")]
+        public string Birthday { get; set; }
+    }
+}
